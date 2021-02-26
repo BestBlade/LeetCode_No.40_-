741c2d7084c242c63f063b97ae5965e0abd0ea60
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-02-26 11:01:47 +0800

    Add files via upload

---
+++ leetcode_no40_组合总和 II_w1.cpp
@@ -0,0 +1,107 @@
+﻿/* ------------------------------------------------------------------|
+给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+
+candidates 中的每个数字在每个组合中只能使用一次。
+
+说明：
+
+所有数字（包括目标数）都是正整数。
+解集不能包含重复的组合。 
+示例 1:
+
+输入: candidates = [10,1,2,7,6,1,5], target = 8,
+所求解集为:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+示例 2:
+
+输入: candidates = [2,5,2,1,2], target = 5,
+所求解集为:
+[
+  [1,2,2],
+  [5]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/combination-sum-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	dfs法
+*
+*	执行用时：8 ms, 在所有 C++ 提交中击败了79.91%的用户
+*	内存消耗：10.4 MB, 在所有 C++ 提交中击败了81.13%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+vector<vector<int>> res;
+vector<int> tmp;
+
+void dfs(int curr, vector<int>& candidates, int target) {
+    if (target == 0) {
+        res.emplace_back(tmp);
+        return;
+    }
+    if (target < 0 || curr == candidates.size()) {
+        return;
+    }
+    
+    for (int i = curr; i < candidates.size(); i++) {
+        if (i > curr && candidates[i] == candidates[i - 1]) {
+            continue;
+        }
+
+        tmp.emplace_back(candidates[i]);
+        dfs(i + 1, candidates, target - candidates[i]);
+        tmp.pop_back();
+    }
+}
+
+vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+    sort(candidates.begin(), candidates.end());
+    dfs(0, candidates, target);
+    return res;
+}
